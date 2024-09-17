0da4e90b7e1fb336bac2b3a6572279e2d67f4ec5
Matthew <defender_0000@mail.ru>
2024-09-17 19:28:51 +0300

    Adding the necessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.20"
 }
 
 group = "ru.matthew"
@@ -10,12 +11,17 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-client-core:2.3.12")
+    implementation("io.ktor:ktor-client-cio:2.3.12")
+    implementation("io.ktor:ktor-client-serialization:2.3.12")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(19)
 }

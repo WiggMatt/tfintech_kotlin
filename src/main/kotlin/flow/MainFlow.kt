6c8fb75722af7b850cf6892ad6964284d83a1779
Matthew <defender_0000@mail.ru>
2024-10-27 17:45:28 +0300

    Improvements. Using an actor

--- src/main/kotlin/flow/MainFlow.kt
+++ src/main/kotlin/flow/MainFlow.kt
@@ -4,7 +4,8 @@ import NewsCsvWriter
 import NewsService
 import controller.NewsApiClient
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.channels.actor
 import models.News
 import org.slf4j.LoggerFactory
 import java.time.LocalDate
@@ -13,7 +14,7 @@ const val WORKER_COUNT = 4
 private val logger = LoggerFactory.getLogger("Main")
 
 @OptIn(DelicateCoroutinesApi::class)
-fun main() = runBlocking {
+fun main(): Unit = runBlocking {
     val workerContext = newFixedThreadPoolContext(WORKER_COUNT, "WorkerPool")
     val newsService = NewsService()
     val apiClient = NewsApiClient()
@@ -21,21 +22,35 @@ fun main() = runBlocking {
     val startDate = LocalDate.of(2024, 9, 16)
     val endDate = LocalDate.of(2024, 12, 17)
 
-    val channel = Channel<List<News>>(Channel.UNLIMITED)
+    val processorActor = newsProcessorActor("news_report.csv")
 
-    val processorJob = CoroutineScope(workerContext).startProcessor(channel, "news_report.csv")
     val workerJobs = List(WORKER_COUNT) { id ->
-        CoroutineScope(workerContext).startWorker(id + 1, channel, newsService, apiClient, startDate, endDate)
+        CoroutineScope(workerContext).startWorker(id + 1, processorActor, newsService, apiClient, startDate, endDate)
     }
 
     workerJobs.forEach { it.join() }
-    channel.close()
-    processorJob.join()
+    processorActor.close()
+}
+
+// Функция для создания актора
+@OptIn(ObsoleteCoroutinesApi::class)
+fun CoroutineScope.newsProcessorActor(outputFilePath: String) = actor<List<News>> {
+    val csvWriter = NewsCsvWriter()
+    try {
+        for (news: List<News> in channel) {
+            logger.info("Processor обрабатывает данные...")
+            csvWriter.saveNewsToCsv(outputFilePath, news)
+        }
+    } catch (e: Exception) {
+        logger.error("Ошибка при обработке данных: ${e.message}", e)
+    } finally {
+        logger.info("Запись в файл завершена.")
+    }
 }
 
 fun CoroutineScope.startWorker(
     id: Int,
-    channel: Channel<List<News>>,
+    processorActor: SendChannel<List<News>>,
     newsService: NewsService,
     apiClient: NewsApiClient,
     startDate: LocalDate,
@@ -50,7 +65,7 @@ fun CoroutineScope.startWorker(
                 logger.info("Worker #$id: Нет новостей на странице $page")
                 break
             }
-            channel.send(news)
+            processorActor.send(news)
             page += WORKER_COUNT
         } catch (e: Exception) {
             logger.error("Worker #$id: Ошибка при загрузке страницы $page: ${e.message}", e)
@@ -59,16 +74,3 @@ fun CoroutineScope.startWorker(
     }
 }
 
-fun CoroutineScope.startProcessor(channel: Channel<List<News>>, outputFilePath: String) = launch {
-    val csvWriter = NewsCsvWriter()
-    try {
-        for (news in channel) {
-            logger.info("Processor обрабатывает данные...")
-            csvWriter.saveNewsToCsv(outputFilePath, news)
-        }
-    } catch (e: Exception) {
-        logger.error("Ошибка при обработке данных: ${e.message}", e)
-    } finally {
-        logger.info("Запись в файл завершена.")
-    }
-}
